3ae9698774dd30c08bfcdd3f814c17fac9e47f50
sujainath03 <sujainath0328@gmail.com>
2026-05-17 12:20:05 +0530

    Update script.js

--- ToDO Frontend/script.js
+++ ToDO Frontend/script.js
@@ -1,5 +1,5 @@
 // Shared configuration
-const SERVER_URL = "https://to-do-application-gg7v.onrender.com";
+const SERVER_URL = "";
 const token = localStorage.getItem("token");
 
 // --- 1. LOGIN LOGIC ---
